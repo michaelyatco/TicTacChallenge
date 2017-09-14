cfae23388ecdc237c32b77b61de6bd65b16ebfb0
michaelyatco <mjyatco@gmail.com>
2017-09-14 09:49:07 -0400

    Move and other turn methods added with tests

--- lib/tictacchallenge.rb
+++ lib/tictacchallenge.rb
@@ -1,9 +1,21 @@
 class Position
   attr_accessor :board, :turn
-  def initialize
+
+  def initialize board=nil, turn="x"
     @dimension = 3
     @size = @dimension * @dimension
-    @board = Array.new(@size, "-")
-    @turn = "x"
+    @board = board || Array.new(@size, "-")
+    @turn = turn
   end
+
+  def move index
+    @board[index] = @turn
+    @turn = other_turn
+    self
+  end
+
+  def other_turn
+    @turn == "x" ? "o" : "x"
+  end
+
 end
--- spec/lib/tictacchallenge_spec.rb
+++ spec/lib/tictacchallenge_spec.rb
@@ -3,12 +3,30 @@ require 'tictacchallenge'
 
 describe Position do
   context "#new" do
-    it "should initialize a new board" do
+    it "initializes a new board" do
       position = Position.new
       expect(position.board).to eq %w(- - -
                                       - - -
                                       - - -)
       expect(position.turn).to eq "x"
+    end
+    it "initializes a position give a board and turn" do
+      position = Position.new(%w(- x -
+                                 - - -
+                                 - o -), "o")
+      expect(position.board).to eq %w(- x -
+                                      - - -
+                                      - o -)
+      expect(position.turn).to eq "o"
+    end
+  end
+  context "#move" do
+    it "makes a move" do
+      position = Position.new.move(0)
+      expect(position.board).to eq %w(x - -
+                                      - - -
+                                      - - -)
+      expect(position.turn).to eq "o"
     end
   end
 end
